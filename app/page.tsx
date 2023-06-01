2f4eb9982ca729e78e71257856b6b7acb1210d8a
Chandra Bhandari <rollinrabin@gmail.com>
2023-06-01 20:48:23 +0545

    file/structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
-import Navbar from './components/Navbar'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
     <> 
-      <Navbar/>
+      <p>hello</p>
     </>
   )
 }
