bb55f3d29818338f6a2381439f4c5a509ecf4b56
Chandra Bhandari <rollinrabin@gmail.com>
2023-06-02 23:50:19 +0545

    feat/meta

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,36 @@
 
 import Navbar from '../components/Navbar'
 import Homepage from '../components/Homepage'
-
+import Head from 'next/head'
 
 export default function Home() {
+  
+ 
+  
+  
   return (
     <> 
+    <Head>
+       
+       <meta name="hotelscout"  key="hostelscout" content="find hostel near you on affordable price in nepal"/>
+     
+       <meta name="description" content="Checkout our hostelscout" key="desc" />
+       <meta property="hostelscout" content="www.facebook.com" />
+       <meta
+         property="og:description"
+         content="And a social description for our website"
+         key="website"
+       />
+       <meta
+          key="property"
+         property="profileimage"
+         content="https://example.com/images/cool-page.jpg"
+       />
+     </Head>
       <Navbar/>
       <Homepage/>
     </>
+
   )
+  
 }
