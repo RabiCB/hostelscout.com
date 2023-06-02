6a6dabacff768aa01ae2c4d54c0ffff1310ac59d
Chandra Bhandari <rollinrabin@gmail.com>
2023-06-02 08:00:13 +0545

    feat/homepage/login

--- dummydata/data.tsx
+++ dummydata/data.tsx
@@ -1,31 +1,51 @@
 export const hosteldata=[{
     id:1,
     name:"square hostel",
+    ownedby:"John Doe",
+    location:"kathmandu ,Nepal",
+    fair:"8000",
     img:"https://images.pexels.com/photos/4217/hotel-bed-bedroom-room.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 },
 {
     id:2,
     name:"square hostel",
-    img:"https://images.pexels.com/photos/4217/hotel-bed-bedroom-room.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    ownedby:"John Doe",
+    fair:"8000",
+    location:"kathmandu ,Nepal",
+    
+    img:"https://images.pexels.com/photos/4217/hotel-bed-bedroom-room.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    
 },
 {
     id:3,
     name:"square hostel",
+    ownedby:"John Doe",
+    fair:"8000",
+    location:"Pokhara ,Nepal",
     img:"https://images.pexels.com/photos/4217/hotel-bed-bedroom-room.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 },
 {
     id:4,
     name:"square hostel",
+    ownedby:"Jane",
+    fair:"8000",
+    location:"kathmandu ,Nepal",
     img:"https://images.pexels.com/photos/4217/hotel-bed-bedroom-room.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 },
 {
     id:5,
     name:"square hostel",
+    ownedby:"John Doe",
+    fair:"8000",
+    location:"kathmandu ,Nepal",
     img:"https://images.pexels.com/photos/4217/hotel-bed-bedroom-room.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 },
 {
     id:6,
     name:"square hostel",
+    ownedby:"John Doe",
+    fair:"8000",
+    location:"kathmandu ,Nepal",
     img:"https://images.pexels.com/photos/4217/hotel-bed-bedroom-room.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 },
 
