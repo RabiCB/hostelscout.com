3f08c24e7958ca39a48109d0f4168296d7e4eb04
Chandra Bhandari <rollinrabin@gmail.com>
2023-06-03 14:51:55 +0545

    feat/skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -3,34 +3,33 @@ import Navbar from '../components/Navbar'
 import Homepage from '../components/Homepage'
 import Head from 'next/head'
 
+
 export default function Home() {
-  
- 
-  
-  
+
   return (
-    <> 
-    <Head>
-       
-       <meta name="hotelscout"  key="hostelscout" content="find hostel near you on affordable price in nepal"/>
-     
-       <meta name="description" content="Checkout our hostelscout" key="desc" />
-       <meta property="hostelscout" content="www.facebook.com" />
-       <meta
-         property="og:description"
-         content="And a social description for our website"
-         key="website"
-       />
-       <meta
+    <>
+      <Head>
+
+        <meta name="hotelscout" key="hostelscout" content="find hostel near you on affordable price in nepal" />
+
+        <meta name="description" content="Checkout our hostelscout" key="desc" />
+        <meta property="hostelscout" content="www.facebook.com" />
+        <meta
+          property="og:description"
+          content="And a social description for our website"
+          key="website"
+        />
+        <meta
           key="property"
-         property="profileimage"
-         content="https://example.com/images/cool-page.jpg"
-       />
-     </Head>
-      <Navbar/>
-      <Homepage/>
+          property="profileimage"
+          content="https://example.com/images/cool-page.jpg"
+        />
+      </Head>
+      <Navbar />
+      <Homepage />
+
     </>
 
   )
-  
+
 }
