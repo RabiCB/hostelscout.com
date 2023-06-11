ec398e360af909f14a3a49663713c6f99ae89482
Chandra Bhandari <rollinrabin@gmail.com>
2023-06-11 18:59:59 +0545

    fix:form

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -8,6 +8,7 @@ import { Button } from "@mui/material"
 import Link from "next/link"
 import { Client, Query, Databases } from "appwrite"
 import Skeletonvariant from "../features/skeleton"
+
 function Homepage() {
 
     const [appwrite, setAppwrite] = useState<any | []>([])
@@ -17,15 +18,11 @@ function Homepage() {
     const [loadmore ,setLoadmore]=useState(false)
      const ref=useRef<HTMLDivElement>(null)
 
-const  refresh=()=>{
-    const lastChildElement = ref.current?.lastElementChild;
-    lastChildElement?.scrollIntoView({ behavior: 'smooth' });
 
-}
     
     const hanldeIncrease=()=>{
         setDocs((prev)=>prev+8)
-        refresh()
+      
         setLoadmore(true)
     }
     
@@ -61,18 +58,14 @@ const  refresh=()=>{
 
     }, [docs])
 
-    useEffect(()=>{
-        const lastChildElement = ref.current?.lastElementChild;
-    lastChildElement?.scrollIntoView({ behavior: 'smooth' });
-    },[docs])
-
+   
                                                               
     return (
-        <div ref={ref}  className="max-sm:grid-cols-1 max-md:grid-cols-2 max-lg:grid-cols-3 h-auto grid px-6 grid-cols-4 gap-6 mt-4 pb-6 relative">
+        <div  className="max-sm:grid-cols-1 max-md:grid-cols-2 max-lg:grid-cols-3 h-auto grid px-6 grid-cols-4 gap-6 mt-4 pb-6 relative">
 
             {
                 appwrite.length>1? appwrite.map((d: any) => (
-                   <Link   href={`/hostel/${d?.$id}`} key={d.hostelname}><div key={d.$id} className=" flex flex-col gap-1 relative">
+                   <Link   href={`/hostel/${d?.$id}`} key={d.hostelname}><div key={d.$id} ref={ref}  className=" flex flex-col gap-1 relative">
 
                         <img src={d.image} className="w-full rounded-lg h-64 aspect-square " alt="hostelimg" />
                         <div className="pl-1 flex flex-col gap-[2px]">
